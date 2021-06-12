3109565bcf9c371544a35a84218c93a2bcfa4fce
namelessto <shaitnto@gmail.com>
2021-06-12 17:06:02 +0300

    added some push notifications support

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:chitchat/utilities/constants.dart';
 import 'package:chitchat/presentation/screens/welcome_screen.dart';
@@ -8,10 +9,19 @@ import 'package:chitchat/presentation/screens/login_screen.dart';
 import 'package:chitchat/presentation/screens/lobby_screen.dart';
 import 'package:chitchat/presentation/screens/chat_screen.dart';
 
+///Receive message when app is terminated
+Future<void> backgroundHandler(RemoteMessage message) async {
+  print(message.data.toString());
+  print(message.notification.title);
+}
+
 Future<void> main() async {
   String initialRoute;
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
+  ///When app is closed
+  FirebaseMessaging.onBackgroundMessage(backgroundHandler);
   if (FirebaseAuth.instance.currentUser != null) {
     initialRoute = lobbyScreenID;
   } else {
@@ -24,13 +34,39 @@ Future<void> main() async {
   );
 }
 
-class Chitchat extends StatelessWidget {
+class Chitchat extends StatefulWidget {
   Chitchat({this.route});
   final String route;
+
+  @override
+  _ChitchatState createState() => _ChitchatState();
+}
+
+class _ChitchatState extends State<Chitchat> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    FirebaseMessaging.instance.getInitialMessage();
+
+    ///foreground
+    FirebaseMessaging.onMessage.listen((message) {
+      if (message.notification != null) {
+        print(message.notification.body);
+        print(message.notification.title);
+      }
+    });
+
+    ///background but running
+    FirebaseMessaging.onMessageOpenedApp.listen((message) {
+      print(message.data["route"]);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: route,
+      initialRoute: widget.route,
       routes: {
         welcomeScreenID: (context) => WelcomeScreen(),
         registrationScreenID: (context) => RegistrationScreen(),
